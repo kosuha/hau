bb749ade0024150ff9bd226d3a7e8dcf781a72b5
Seonho Kim <okeydokekim@gmail.com>
2025-04-09 21:08:40 +0900

    server cicd setting

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const requestHandler = (req, res) => {
+  console.log(`요청 URL: ${req.url}`);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, Node.js Server!\n');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(PORT, () => {
+  console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
+});
